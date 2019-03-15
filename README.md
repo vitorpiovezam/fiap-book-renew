2801283ad75a0c61b456b2190c54028aeb433d3a
unknown <vitorpiovezam@yandex.com>
2019-03-15 13:17:27 -0300

    Init
    Signed-off-by: unknown <vitorpiovezam@yandex.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## fiap-book-renew
+  <img src="https://user-images.githubusercontent.com/10379601/29446482-04f7036a-841f-11e7-9872-91d1fc2ea683.png" height="109" align="right">
+I have always forgotten to renew the books borrowed from my college.
+So I made this code that on the college website, logs in and renews all the borrowed books.
+
+### Install and run 
+Install dependecies
+```
+$ npm install
+```
+In index.js, pass 2 arguments in renewBooks method, string login and string password from your user on college website.
+```
+ await renewBooks('78212','986588');
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const user = 'YOUR LOGIN GOES HERE'
+  const password = 'YOUR PASSWORD GOES HERE'
+  await renewBooks('','160499');
+})();
+
+async function renewBooks(rm,password) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://www2.fiap.com.br/", { waitUntil: "networkidle2" });
+
+  await page.focus("input[type=text]#usuario");
+  await page.keyboard.type(rm);
+
+  await page.focus("input[type=password]#senha");
+  await page.keyboard.type(password);
+
+  await page.click(".a-login-btn");
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+  await page.goto(
+    "https://www2.fiap.com.br/programas/login/alunos_2004/biblioteca",
+    { waitUntil: "networkidle2" }
+  );
+
+  await clickByText(page,'Meus Empréstimos');
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+  await page.click('input.i-content-btn');
+  await page.pdf({ path: "my_books.pdf", format: "A4" });
+
+  await browser.close();
+}
+
+const escapeXpathString = str => {
+    const splitedQuotes = str.replace(/'/g, `', "'", '`);
+    return `concat('${splitedQuotes}', '')`;
+  };
+
+const clickByText = async (page, text) => {
+    const escapedText = escapeXpathString(text);
+    const linkHandlers = await page.$x(`//a[contains(text(), ${escapedText})]`);
+    
+    if (linkHandlers.length > 0) {
+      await linkHandlers[0].click();
+    } else {
+      throw new Error(`Link not found: ${text}`);
+    }
+};
